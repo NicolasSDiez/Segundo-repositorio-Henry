a359e027fc710fa2226a6a913eb8d268fb08e1b6
NicolasSDiez <nicolas.diez1s@gmail.com>
2023-09-21 12:26:49 -0300

    21/09/23

--- FT-M1-master/FT-M1-master/07-Algoritmos-I/homework/homework.js
+++ FT-M1-master/FT-M1-master/07-Algoritmos-I/homework/homework.js
@@ -7,6 +7,21 @@ function factorear(num) {
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
 
+  //empieza a dividir
+  let arrayFactores = [1]
+  let divisor = 2;
+  while(num > 1){
+    if(num % divisor === 0){
+      //pushear
+      arrayFactores.push(divisor)
+      num /= divisor
+    }
+    //aumenta el divisor
+    else {
+      divisor++
+    }
+  }
+return arrayFactores;
 }
 
 function bubbleSort(array) {
@@ -14,7 +29,16 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      let aux = array[j]
+      if(array[j] > array[j+1]){
+        array[j] = array[j+1]
+        array[j+1] = aux
+      }
+    }
+  }
+  return array
 }
 
 
@@ -23,7 +47,18 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+  for (let i = 1; i < array.length; i++){
+    let aux = array[i]
+    let j = i-1;
 
+
+    while(j >= 0 && aux < array[j]){
+      array[j+1] = array[j]
+      j--;
+    }
+    array[j+1] = aux
+  }
+  return array
 }
 
 
@@ -32,7 +67,19 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 0; i < array.length; i++){
+    //identifico al menor valor
+    let minimo = i
+    for (let j = i + 1; j < array.length; j++){
+      if(array[j] < array[minimo]){
+        minimo = j
+      }
+    }
+    let aux = array[i]
+    array[i] = array[minimo]
+    array[minimo] = aux
+  }
+  return array
 }
 
 
