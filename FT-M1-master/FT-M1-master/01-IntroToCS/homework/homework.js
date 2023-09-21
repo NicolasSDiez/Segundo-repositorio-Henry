bf874531f2305fa840ca2866c3153789c6dfc01e
NicolasSDiez <nicolas.diez1s@gmail.com>
2023-09-21 20:56:40 -0300

    21/09/23

--- FT-M1-master/FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/FT-M1-master/01-IntroToCS/homework/homework.js
@@ -1,9 +1,26 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   let resultado = 0;
+   let posicion = num.length -1;
+   for(let i = 0; i<num.length; i++){
+      resultado = resultado + num[i] * 2 ** posicion;
+      posicion--;
+   }
+   return resultado;
+}
 
-function DecimalABinario(num) {}
 
+
+function DecimalABinario(num) {
+   let binario = [];
+   while(num > 0){
+      let resto = num % 2;
+      binario.unshift(resto)
+      num = Math.floor(num / 2);
+   }
+   return binario.join('');
+}
 module.exports = {
    BinarioADecimal,
    DecimalABinario,
